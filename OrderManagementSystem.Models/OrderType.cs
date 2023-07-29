327ae1cfc9deca1d8e9fbaf9358b0a419b58c90c
Віктор Липкань <lipkan02@gmail.com>
2023-07-29 22:39:53 +0300

    add order type model

---
+++ OrderManagementSystem.Models/OrderType.cs
@@ -0,0 +1,7 @@
+﻿namespace OrderManagementSystem.Models;
+
+public class OrderType : BaseEntity
+{
+    public string? Name { get; set; }
+    public ICollection<Order>? Orders { get; set; } = new HashSet<Order>();
+}
