6b3b8fee89f4bb9779ceec11b5f96093f06c00f8
Abdul4ik02 <lipkan02@gmail.com>
2023-08-05 22:40:14 +0300

    update code structure

--- OrderManagementSystem.Web/PaginatedList.cs
+++ OrderManagementSystem.Web/PaginatedList.cs
@@ -6,6 +6,8 @@ public class PaginatedList<T> : List<T>
 {
     public int PageIndex { get; private set; }
     public static int TotalPages { get; private set; }
+    public bool HasPreviousPage => PageIndex > 1;
+    public bool HasNextPage => PageIndex < TotalPages;
 
     public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
     {
@@ -15,8 +17,6 @@ public class PaginatedList<T> : List<T>
         this.AddRange(items);
     }
 
-    public bool HasPreviousPage => PageIndex > 1;
-    public bool HasNextPage => PageIndex < TotalPages;
     public static PaginatedList<T> Create(IEnumerable<T> source, int pageIndex, int pageSize)
     {
         var count = source.Count();
