4a49ff722502bb1ef360f056a835dd380a12a677
Віктор Липкань <lipkan02@gmail.com>
2024-11-28 22:48:56 +0200

    add worklog repository

---
+++ OrderManagementSystem.Data/Data/WorkLogRepository.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Core;
+using Domain.WorkLogs;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class WorkLogRepository : Repository<WorkLog>, IWorkLogRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public WorkLogRepository(AppDbContext dbContext) : base(dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<IEnumerable<WorkLog>> GetAllAsync(CancellationToken token)
+    {
+        return await _dbContext.WorkLogs
+            .Include(w => w.Customer)
+            .Include(w => w.WorkType)
+            .ToArrayAsync(token);
+    }
+
+    public new async Task<WorkLog?> GetAsync(int id, CancellationToken token)
+    {
+        return await _dbContext.WorkLogs
+            .Where(w => w.Id == id)
+            .Include(w => w.Customer)
+            .Include(w => w.WorkType)
+            .FirstOrDefaultAsync(token);
+    }
+}
---
+++ OrderManagementSystem.Models/WorkLogs/IWorkLogRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Abstract;
+
+namespace Domain.WorkLogs;
+
+public interface IWorkLogRepository : IRepository<WorkLog>
+{
+    Task<IEnumerable<WorkLog>> GetAllAsync(CancellationToken token);
+    new Task<WorkLog?> GetAsync(int id, CancellationToken token);
+}
